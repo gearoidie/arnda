5fd2e49a4b544b5726fb706334ac7bf40ce3300a
Gearóid Crowley <gearoidie@gmail.com>
2018-03-24 17:20:05 +0200

    Setting up git fetch

--- src/ardna.ts
+++ src/ardna.ts
@@ -2,7 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
+import * as cp from 'child_process';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -14,15 +14,19 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('ardna.sayHello', () => {
+    let disposable = vscode.commands.registerCommand('ardna.fetch', () => {
         // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello Gearoid!');
+        cp.exec("cd " + vscode.workspace.rootPath + " git fetch", (err:Error, stdout:string, stderr:string) => {
+         // Display a message box to the user
+        vscode.window.showInformationMessage(stdout);
+        
+        if (err) {
+            vscode.window.showErrorMessage(err.message);
+        }
+        });
 
     });
 
-    
     context.subscriptions.push(disposable);
 }
 
